e17ef9b32fc72c35ade0b1779cc121d46c709475
NataschaBGB <nannabjerning@hotmail.com>
2025-12-10 10:12:26 +0100

    add: mixin for button style to reuse on all buttons

--- src/components/PopularProducts/PopularProducts.jsx
+++ src/components/PopularProducts/PopularProducts.jsx
@@ -5,10 +5,13 @@ import './PopularProducts.sass'
 export default function PopularProducts() {
     return (
         <section className="popular-products">
-            <h2 className="popular-products__title">Popular Products</h2>
-            <Link to={"/products"} className="popular-products__view-all">
-                See all products
-            </Link>
+            <section className="popular-products__header">
+                <h2 className="popular-products__title">Popular Products</h2>
+                <Link to={"/products"} className="popular-products__button">
+                    See all products
+                </Link>
+            </section>
+            
             <div className="popular-products__items">
                 {/* Product items would go here */}
             </div>
