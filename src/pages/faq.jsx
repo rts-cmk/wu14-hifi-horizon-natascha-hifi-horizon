e315c068aed9d2a508d0fd9305652dbe9f24a26b
NataschaBGB <nannabjerning@hotmail.com>
2025-12-15 10:45:27 +0100

    add: FAQ page

---
+++ src/pages/faq.jsx
@@ -0,0 +1,23 @@
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
+import '../style/_layout.sass'
+
+
+export default function FAQ() {
+
+    return (
+        <>
+            <Header />
+            
+            <main className="faq">
+
+                <h1>FAQ</h1>
+
+                
+
+            </main>
+            
+            <Footer />
+        </>
+    )
+}
