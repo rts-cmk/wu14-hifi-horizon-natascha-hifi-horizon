039cd037fae8087c23929a41451bca98927b5e44
NataschaBGB <nannabjerning@hotmail.com>
2025-12-16 11:59:51 +0100

    fix: minor styling details

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -4,12 +4,14 @@ import './Newsletter.sass';
 export default function Newsletter() {
     return (
         <section className="newsletter">
-            <h2 className="newsletter__title">SIGN UP FOR OUR NEWSLETTER</h2>
-            <p className="newsletter__description">Subscribing to our newsletter secures you up to date information about HiFi Horizons latest updates and offers.</p>
-            <form className="newsletter__form">
-                <input type="email" className="newsletter__input" placeholder='Email' required/>
-                <button type="submit" className="newsletter__button">Sign up</button>
-            </form>
+            <section className="newsletter__container">
+                <h2 className="newsletter__title">SIGN UP FOR OUR NEWSLETTER</h2>
+                <p className="newsletter__description">Subscribing to our newsletter secures you up to date information about HiFi Horizons latest updates and offers.</p>
+                <form className="newsletter__form">
+                    <input type="email" className="newsletter__input" placeholder='Email' required/>
+                    <button type="submit" className="newsletter__button">Sign up</button>
+                </form>
+            </section>
         </section>
     );
 }
